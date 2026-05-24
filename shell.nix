d68351c0a367f23252ac555cb12b9707cbded094
grapeco <mihajlovs928@gmail.com>
2026-05-24 21:35:16 +0300

    Refactoring shell.nix

--- shell.nix
+++ shell.nix
@@ -1,27 +1,24 @@
 { pkgs ? import <nixpkgs> {} }:
 
 with pkgs;
-let 
-  depend = with pkgs; [
+mkShell rec {
+  nativeBuildInputs = [ 
+    pkg-config 
+    wrapGAppsHook3
+  ];
+  
+  buildInputs = with pkgs; [
     (fenix.combine [
       fenix.complete.toolchain
     ])
     
     gtk3
-    papirus-icon-theme
     glib
   ]; 
-in mkShell {
-  nativeBuildInputs = [ 
-    pkg-config 
-    wrapGAppsHook3
-  ];
-  buildInputs = depend;
 
   shellHook = ''
     export PATH=$PATH:''${CARGO_HOME:~/.cargo}/bin
-    export XDG_DATA_DIRS=$GSETTINGS_SCHEMAS_PATH
   '';
   
-  LD_LIBRARY_PATH = lib.makeLibraryPath depend;
+  LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
 }
